c5171d4e89a4b0249b197b1b3144ce6724b0ebc0
fabien1981 <166442156+fabien1981@users.noreply.github.com>
2024-09-17 21:41:05 +0200

    ajout page signin

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,9 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/habitats", "Habitats", "/pages/habitats.html"),];
+    new Route("/habitats", "Habitats", "/pages/habitats.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/services", "Services", "/pages/services.html"),];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Arcadia";
