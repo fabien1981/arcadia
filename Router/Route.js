101119eb5356f211d3194d932c45995f060ee8fa
fabien1981 <166442156+fabien1981@users.noreply.github.com>
2024-09-19 15:00:55 +0200

    creation role

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,18 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize;
     }
 }
+
+/*
+[]-> Tout le monde peut y acceder
+["disconnected"] -> Réservé aux utilisateurs deconnectés
+["admin"] -> Réservé à l'administrateur
+["veterinaire"] -> Réservé aux veterinaires
+["employe"] -> Réservé aux employés
+["admin","employe","veterinaire"] ->Réservé aux utilsateurs admin, employé et veterinaire
+*/
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,11 +2,15 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/habitats", "Habitats", "/pages/habitats.html"),
-    new Route("/signin", "Connexion", "/pages/signin.html", "/js/signin.js"),
-    new Route("/services", "Services", "/pages/services.html"),
-    new Route("/contact", "Contact", "/pages/contact.html","/js/contact.js"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/habitats", "Habitats", "/pages/habitats.html",[]),
+    new Route("/signin", "Connexion", "/pages/signin.html",["disconnected"], "/js/signin.js"),
+    new Route("/services", "Services", "/pages/services.html",[]),
+    new Route("/contact", "Contact", "/pages/contact.html",[],"/js/contact.js"),
+    new Route("/avis", "avis", "/pages/avis.html",[],"/js/avis.js"),
+    new Route("/admin", "administrateur", "/pages/admin.html",["admin"],"/js/admin.js"),
+    new Route("/veterinaire", "veterinaire", "/pages/veterinaire.html",[],"/js/veterinaire.js"),
+    new Route("/employe", "employe", "/pages/employe.html",[],"/js/employe.js"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
